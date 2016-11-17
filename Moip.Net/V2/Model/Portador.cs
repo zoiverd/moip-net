0bcd68bd765ae20a900a08f607f8723ce8f8af94
Michael Oliveira <michaeloc9@gmail.com>
2016-11-17 15:35:31 -0300

    Mudança do atributo BirthDate para Birthdate na classe Portador

--- Moip.Net/V2/Model/Portador.cs
+++ Moip.Net/V2/Model/Portador.cs
@@ -5,7 +5,7 @@ namespace Moip.Net.V2.Model
     public class Portador
     {
         public string Fullname { get; set; }
-        public string BirthDate { get; set; }
+        public string Birthdate { get; set; }
         public Telefone Phone { get; set; }
         public Documento TaxDocument { get; set; }
         public Endereco BillingAddress { get; set; }
